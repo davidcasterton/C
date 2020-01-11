bc3c84c17fd569896036d4e2c75337520e03ff88
David Casterton <dcasterton@zoox.com>
2020-01-10 17:00:01 -0800

    daveb update

--- questions/greater_than_fifty_percent.py
+++ questions/greater_than_fifty_percent.py
@@ -1,41 +1,55 @@
+import csv
 import datetime
+import itertools
 import json
+import os
 import pandas
 import pdb
+import pprint
 
 
-df = pandas.read_table('daveb_data_2.txt')
+THRESHOLD = 50
+INPUT_FILE_PATH = '/home/dave/Downloads/list for dave.txt'
+OUTPUT_FILE_PATH = os.path.abspath('%s.csv' % datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+COL_NAME = 'Overarching Entity (Short Name)'
+COL_SHARES = 'Shares of A+B+C'
+COL_PERCENT = '% of (A+B+C)'
+
+
+df = pandas.read_table(INPUT_FILE_PATH)
 df = df.iloc[:, range(3)]
-col_name = 'Overarching Entity (Short Name)'
-col_shares = 'Shares of A+B+C'
-col_percent = '% of (A+B+C)'
-df[col_percent] = df[col_percent].map(lambda x: x.rstrip('%'))
-df[col_percent] = df[col_percent].astype(float)
-df.sort_values(by=col_percent, ascending=False, inplace=True)
+df[COL_PERCENT] = df[COL_PERCENT].map(lambda x: x.rstrip('%'))
+df[COL_PERCENT] = df[COL_PERCENT].astype(float)
+df.sort_values(by=COL_PERCENT, ascending=False, inplace=True)
 
+#unique_combinations = []
+num_results = 0
 
-total = sum([float(x) for x in df[col_percent].values])
-threshold = 50
+with open(OUTPUT_FILE_PATH, 'w') as f:
+    csv_writer = csv.writer(f, delimiter=',')
 
-results = {
-    'sum of all percents': total,
-    'threshold' : threshold
-}
-for i in range(len(df)):
-    for k in range(i, len(df)):
-        percents = df[col_percent][i:k].values
-        if sum(percents) >= threshold:
-            result = {
-                'sum': sum(percents), 
-                'names': list(df[col_name][i:k].values),
-                'shares': list(df[col_shares][i:k].values),
-                'percents': list(percents)
-            }
-            try:
-                results[len(percents)].append(result)
-            except KeyError as e:
-                results[len(percents)] = []
-                results[len(percents)].append(result)
+    header = ['num', 'total percent']
+    for i in range(len(df)):
+        header.append("%s (%s)" % (df[COL_NAME][i], df[COL_PERCENT][i]))
+    csv_writer.writerow(header)
 
-with open('%s.json' % datetime.datetime.now(), 'w') as f:
-    json.dump(results, f, indent=4)
+    for sequence_length in range(1, 30):
+        print(sequence_length)
+        
+        for indexes in itertools.combinations(range(len(df)), sequence_length):
+            percents = df[COL_PERCENT][list(indexes)].values
+
+            if sum(percents) >= THRESHOLD:
+                num_results += 1
+
+                row = [sequence_length, sum(percents)]
+                funds = [""] * len(df)
+                for index in indexes:
+                    funds[index] = "x"
+                row.extend(funds)
+
+                csv_writer.writerow(row)
+
+                print("%s : %s" % (sequence_length, num_results))
+
+print(OUTPUT_FILE_PATH)
